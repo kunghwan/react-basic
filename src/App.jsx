d8784ad8061e35d457ec78603ad5f5af33b75358
kunghwan <ysw03031@naver.com>
2025-02-03 10:26:43 +0900

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
-import { Meat } from "./Food";
-import Hobby from "./Hobby";
-import { Contact, Country } from "./Info";
-import MyName from "./MyName";
-
 const App = () => {
   return (
     <div>
-      <h1>app</h1>
-      <MyName />
-      <Contact />
-      <Country />
-      <Meat />
-      <Hobby />
+      <h1>React Tutorial</h1>
     </div>
   );
 };
