efb8ff4116277555efdf0ae0b575af46b5f24aff
ykh <ysw03031@naver.com>
2025-02-09 21:10:24 +0900

    homework todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // 1. react에서 useState, useEffect, useRef 꺼내오기
 
 import { useState, useEffect, useRef } from "react";
@@ -14,88 +13,13 @@ const App = () => {
   // 4. useState로 todo 선언하고 초기값은 빈 문자열
   const [todo, setTodo] = useState("");
 
-  const inputref = useRef(null);
-
-  // 6. onSubmit 이라는 이름의 함수를 만드세요.
-
-  const onSubmit = (e) => {
-    e.preventDefault();
-    // 6-2. todo 값 입력되지 않았을 떄 예외처리
-    //  예외처리 경고 + (인풋창 포커스)
-    if (todo.length === 0) {
-      inputref?.current.focus();
-      return alert("입력해줘");
-    }
-    if (todo.length <= 1) {
-      return alert("1글자도 안됨");
-    }
-    setTodos((prev) => [...prev, todo]);
-    setTodo("");
-    // set함수는 무조건 원래의 값과 같은 탑입을 return해줘야함
-    // 6-3. todo값이 1글자 이하일 떄에도 예외처리
-    // 6-4 setTodo함수를 사용해서 todo 값을 todos에 추가하기
-  };
-
   useEffect(() => {
-<<<<<<< HEAD
     console.log(todo);
   }, [todo]);
-=======
-import { useState, useEffect } from "react";
-
-const App = () => {
-  const [sec, setSec] = useState(0);
-  const [counting, setCounting] = useState(false);
-
-  const onPlus = () => {
-    setSec((prev) => prev + 1000);
-  };
-  const onMinus = () => {
-    setSec((prev) => prev - 1000);
-  };
-
-  const onReset = () => {
-    setSec(0);
-  };
-
-  useEffect(() => {
-    console.log(sec);
-  }, [sec]);
-  // [] 빈배열은 최초 1회만 실행 해당 컴포넌트가 렌더링 되는 시점
-
-  useEffect(() => {
-    if (counting) {
-      const count = setInterval(() => {
-        setSec((prev) => prev + 10);
-      }, 10);
-
-      return () => {
-        clearInterval(count);
-      };
-    }
-  }, [counting]);
-
-  useEffect(() => {
-    console.log("app has mounted!");
-  }, []);
-
-  // 배열을 사용하지 않으면 눈치개코가 됨
-  // 모든 것들에 반응하는 흉악한 코드가 될 수 있음
-  useEffect(() => {
-    console.log("sdfsdfsdfdf");
-  });
-
-  // 배열에 우리가 주시하고 싶은 변수/함수를 넣어두면 해당 변수/함수가 업데이트 될 때에만 작동하는 코드를 짤 수 있음.
->>>>>>> caa283e504f66e820c3f060192c55224b89460aa
-=======
-    console.log(todos);
-  }, [todos]);
->>>>>>> 85820312b006fe387f36f93f8626b6d9c25bac61
 
   return (
     <div>
       <h1>App</h1>
-<<<<<<< HEAD
       {/* 5.form 만들고 안에 input, button 넣기 
       
       5-2. input에 todo 연결하기 value, onChange
@@ -104,51 +28,14 @@ const App = () => {
       
       */}
 
-      <form action="" onSubmit={onSubmit}>
+      <form action="" onSubmit={(e) => e.preventDefault()}>
         <input
           type="text"
           value={todo}
           onChange={(e) => setTodo(e.target.value)}
-          ref={inputref}
         />
-        <button>ADD</button>
+        <button></button>
       </form>
-<<<<<<< HEAD
-=======
-      <h3>{sec / 1000}초</h3>
-      <button onClick={onPlus}>+</button>
-      <button onClick={onMinus}>-</button>
-      <button onClick={onReset}>reset</button>
-      <button
-        onClick={() => {
-          setCounting((prev) => !prev);
-        }}
-      >
-        {counting ? "stop" : "start"}
-      </button>
->>>>>>> caa283e504f66e820c3f060192c55224b89460aa
-=======
-      <ul>
-        {todos.map((to) => {
-          const onDelete = () =>
-            setTodos((prev) => prev.filter((item) => item !== to));
-
-          // setTodos((prev) => {
-          //     // let copy = [...prev];
-          // // copy.splice(index, 1);
-          // // return copy;\
-
-          // });
-
-          return (
-            <li key={to}>
-              {to}
-              <button onClick={onDelete}>삭제</button>
-            </li>
-          );
-        })}
-      </ul>
->>>>>>> 85820312b006fe387f36f93f8626b6d9c25bac61
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -2,25 +2,21 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-<<<<<<< HEAD
 import Verify from "./Verify.jsx";
 import Timer from "./Timer.jsx";
 import Code from "./Code.jsx";
-=======
+import RemindTodo from "./todo/RemindTodo.jsx";
 import Todo from "./todo/Todo.jsx";
->>>>>>> 85820312b006fe387f36f93f8626b6d9c25bac61
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
-<<<<<<< HEAD
     {/* <Verify /> */}
     <>
-      <Timer />
-      <Code />
+      {/* <Timer />
+      <Code /> */}
     </>
-=======
     <Todo />
->>>>>>> 85820312b006fe387f36f93f8626b6d9c25bac61
+    <RemindTodo />
   </StrictMode>
 );
